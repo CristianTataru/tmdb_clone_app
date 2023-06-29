fd0770592df54f3e23cb326e088908190c5015ba
CristianTataru <tatarucristiangabriel1993@gmail.com>
2023-06-29 10:35:55 +0300

    Cleanup

--- lib/di/di_container.dart
+++ lib/di/di_container.dart
@@ -5,6 +5,25 @@ import 'package:tmdb_clone_app/di/di_container.config.dart';
 final diContainer = GetIt.instance;
 
 @InjectableInit()
-void configureDependencies() {
-  diContainer.init();
-}
+void configureDependencies() => diContainer.init();
+
+// void configureDependencies() {
+//   diContainer.registerSingleton<Dio>(
+//     Dio()
+//       ..interceptors.addAll(
+//         [
+//           AppendHeadersInterceptor(),
+//           if (!kReleaseMode) LoggingInterceptor(),
+//           if (Platform.environment.containsKey('FLUTTER_TEST')) MockBackendInterceptor(),
+//         ],
+//       ),
+//   );
+//   diContainer.registerSingleton<TMDBApi>(TMDBApi(diContainer.get()));
+//   diContainer.registerSingleton<TMDBRepository>(TMDBRepository(diContainer.get()));
+//   diContainer.registerFactory<PopularMoviesBloc>(() => PopularMoviesBloc(diContainer.get()));
+//   diContainer.registerFactory<HomeBloc>(() => HomeBloc(diContainer.get(), diContainer.get()));
+//   diContainer.registerFactory<MovieDetailsBloc>(() => MovieDetailsBloc(diContainer.get(), diContainer.get()));
+//   diContainer.registerFactory<PersonDetailsBloc>(() => PersonDetailsBloc(diContainer.get(), diContainer.get()));
+//   diContainer.registerFactory<TrendingMoviesBloc>(() => TrendingMoviesBloc(diContainer.get()));
+//   diContainer.registerSingleton<AppRouter>(AppRouter());
+// }
