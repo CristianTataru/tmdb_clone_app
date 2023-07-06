e25590b09242d5cf9986740131304ffdfc2608d3
CristianTataru <tatarucristiangabriel1993@gmail.com>
2023-07-06 16:12:00 +0300

    test_ci

--- test/bdd/step/the_app_is_running.dart
+++ test/bdd/step/the_app_is_running.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tmdb_clone_app/di/di_container.dart';
 import 'package:tmdb_clone_app/main.dart';
 
+import '../../helpers/mock_http_overrides.dart';
+
 Future<void> theAppIsRunning(WidgetTester tester) async {
+  HttpOverrides.global = MockHttpOverrides();
   configureDependencies();
   await tester.pumpWidget(const MyApp());
 }
